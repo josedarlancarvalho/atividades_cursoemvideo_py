51471aad621792f86010f89541ad97ef6e054960
Darlan Carvalho <137117083+josedarlancarvalho@users.noreply.github.com>
2024-06-22 15:50:37 -0300

    Add files via upload

---
+++ 02/teste.py
@@ -0,0 +1,11 @@
+import math
+
+n = float(input("digite um numero: "))
+inteiro = math.trunc(n)
+
+print(inteiro)
+
+#primeiro teste de biblioteca. 
+#esse consiste em mostrar o numero inteiro do numero que foi digitado
+# exemplo: o usuario digitou 5.3
+# a saida sera = 5
