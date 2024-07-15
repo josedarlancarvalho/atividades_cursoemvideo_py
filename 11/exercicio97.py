c0db98aa65be00d23bd757d36706b2238d019766
Darlan Carvalho <137117083+josedarlancarvalho@users.noreply.github.com>
2024-07-15 15:48:06 -0300

    t

--- 11/exercicio97.py
+++
@@ -1,9 +0,0 @@
-'''Exercício Python 097: Faça um programa que tenha uma função chamada escreva(),
-que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.      '''
-def linha(frase):
-    print('-' * (len(frase)))
-    print(f'{  frase}')
-    print('-' * (len(frase)))
-
-a = str(input("digite a frase:"))
-linha(a)
